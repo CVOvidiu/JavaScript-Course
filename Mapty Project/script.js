10b1f1a44ffc0ae5c03698824da38d2afbae7bec
CVOvidiu <cvovidiu2002@gmail.com>
2022-12-02 14:56:22 +0200

    Managing Workout Data

--- Mapty Project/script.js
+++ Mapty Project/script.js
@@ -11,6 +11,45 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = Date.now().toString().slice(-10);
+
+  constructor(coords, dist, dur) {
+    this.coords = coords; // [lat, lng]
+    this.dist = dist; // in km
+    this.dur = dur; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, dist, dur, cadence) {
+    super(coords, dist, dur);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.dur / this.dist;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, dist, dur, elevationGain) {
+    super(coords, dist, dur);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.dist / (this.dur / 60);
+    return this.speed;
+  }
+}
+
 class App {
   #map;
   #mapEvent;
